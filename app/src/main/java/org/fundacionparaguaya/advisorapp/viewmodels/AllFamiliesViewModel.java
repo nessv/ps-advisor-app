74506f815dbf03a0db59b805da04dc1e3c041dac
melokda <melokda@wpi.edu>
2018-01-13 16:32:42 -0500

    Added cards to the layout and started view model

--- app/src/main/java/org/fundacionparaguaya/advisorapp/viewmodels/AllFamiliesViewModel.java
+++ app/src/main/java/org/fundacionparaguaya/advisorapp/viewmodels/AllFamiliesViewModel.java
@@ -1,11 +1,28 @@
 package org.fundacionparaguaya.advisorapp.viewmodels;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+
+import org.fundacionparaguaya.advisorapp.models.Family;
+
+import java.util.List;
 
 /**
  * Created by Mone Elokda on 1/13/2018.
  */
 
 public class AllFamiliesViewModel extends ViewModel {
+    private MutableLiveData<List<Family>> families;
+    public LiveData<List<Family>> getFamily(){
+        if(families == null){
+            families = new MutableLiveData<List<Family>>();
+        }
+        return families;
+    }
+
+    private void loadfamilies(){
+        
+    }
 
 }

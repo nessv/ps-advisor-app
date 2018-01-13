5be98784e3d873de89a4250321b0ed79e7cb9b6e
melokda <melokda@wpi.edu>
2018-01-13 14:29:22 -0500

    Started layout for all families fragment and card view

---
+++ app/src/main/java/org/fundacionparaguaya/advisorapp/fragments/AllFamiliesFragment.java
@@ -0,0 +1,10 @@
+package org.fundacionparaguaya.advisorapp.fragments;
+
+import android.app.Fragment;
+
+/**
+ * Created by Mone Elokda on 1/13/2018.
+ */
+
+public class AllFamiliesFragment extends Fragment {
+}
---
+++ app/src/main/java/org/fundacionparaguaya/advisorapp/viewmodels/AllFamiliesViewModel.java
@@ -0,0 +1,11 @@
+package org.fundacionparaguaya.advisorapp.viewmodels;
+
+import android.arch.lifecycle.ViewModel;
+
+/**
+ * Created by Mone Elokda on 1/13/2018.
+ */
+
+public class AllFamiliesViewModel extends ViewModel {
+
+}

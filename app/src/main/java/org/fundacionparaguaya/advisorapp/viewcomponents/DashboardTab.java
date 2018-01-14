a101e62df6216d00a7bb6bf66cb6f07e4dc6ae34
Alex Tacescu <alextac98@gmail.com>
2018-01-14 11:43:47 -0500

    Add custom Attributes to DashboardTab

--- app/src/main/java/org/fundacionparaguaya/advisorapp/viewcomponents/DashboardTab.java
+++ app/src/main/java/org/fundacionparaguaya/advisorapp/viewcomponents/DashboardTab.java
@@ -1,6 +1,7 @@
 package org.fundacionparaguaya.advisorapp.viewcomponents;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.util.AttributeSet;
 import android.widget.ImageView;
@@ -19,26 +20,22 @@ public class DashboardTab extends LinearLayoutCompat {
 
     public DashboardTab(Context context, AttributeSet attributeSet){
         super(context, attributeSet);
-    }
+        TypedArray a = context.getTheme().obtainStyledAttributes(attributeSet, R.styleable.DashboardTab,0, 0);
 
-    public DashboardTab(Context context, AttributeSet attributeSet, int imageID, String title){
-        super(context, attributeSet);
-        image_icon = (ImageView) findViewById(R.id.imageView_icon);
-        textView_caption = (TextView) findViewById(R.id.textView_caption);
-        setImage(imageID);
-        setText(title);
-    }
-
-    public void setSelected(boolean selected){
-
+        setImage(a.getResourceId(R.styleable.DashboardTab_ImageID, 0)); //TODO: change default values
+        setText(a.getResourceId(R.styleable.DashboardTab_TextID, 0));
     }
 
     public void setImage(int id) {
         image_icon.setImageResource(id);
     }
 
-    public void setText(String caption){
+    public void setText_string(String caption){
         textView_caption.setText(caption);
+    }
+
+    public void setText(int id){
+        textView_caption.setText(id);
     }
 
 }

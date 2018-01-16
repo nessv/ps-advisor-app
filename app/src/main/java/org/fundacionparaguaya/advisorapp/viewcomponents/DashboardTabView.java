70b1ec0388aaeb97fafb417abdf2ebea0672fbce
Alex Tacescu <alextac98@gmail.com>
2018-01-16 01:06:07 -0500

    Add tab click support

--- app/src/main/java/org/fundacionparaguaya/advisorapp/viewcomponents/DashboardTabView.java
+++ app/src/main/java/org/fundacionparaguaya/advisorapp/viewcomponents/DashboardTabView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.media.Image;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -24,8 +25,13 @@ public class DashboardTabView extends LinearLayout {
     DashboardTab archiveTab;
     DashboardTab settingsTab;
     ImageButton bugButton;
+
+
     public DashboardTabView(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        inflater.inflate(R.layout.dashboardtabview, this);
 
         dashboardTabView = (LinearLayout) findViewById(R.id.dashboardtabview);
         fpLogo = (ImageView) findViewById(R.id.fp_logo);
@@ -35,10 +41,44 @@ public class DashboardTabView extends LinearLayout {
         settingsTab = (DashboardTab) findViewById(R.id.settings_tab);
         bugButton = (ImageButton) findViewById(R.id.bug_button);
 
-        LayoutInflater inflater = LayoutInflater.from(context);
-        inflater.inflate(R.layout.dashboardtabview, this);
-
+        familyTab.setOnClickListener(clickListener);
+        mapTab.setOnClickListener(clickListener);
+        archiveTab.setOnClickListener(clickListener);
+        settingsTab.setOnClickListener(clickListener);
     }
 
+    final OnClickListener clickListener = new OnClickListener() {
+        @Override
+        public void onClick(final View v) {
+            switch (v.getId()){
+                case R.id.family_tab:
+                    familyTab.setSelected(true);
+                    mapTab.setSelected(false);
+                    archiveTab.setSelected(false);
+                    settingsTab.setSelected(false);
+                    break;
+                case R.id.map_tab:
+                    familyTab.setSelected(false);
+                    mapTab.setSelected(true);
+                    archiveTab.setSelected(false);
+                    settingsTab.setSelected(false);
+                    break;
+                case R.id.archive_tab:
+                    familyTab.setSelected(false);
+                    mapTab.setSelected(false);
+                    archiveTab.setSelected(true);
+                    settingsTab.setSelected(false);
+                    break;
+                case R.id.settings_tab:
+                    familyTab.setSelected(false);
+                    mapTab.setSelected(false);
+                    archiveTab.setSelected(false);
+                    settingsTab.setSelected(true);
+                    break;
+                default:
 
+                    break;
+            }
+        }
+    };
 }

f9813af36be4a92e4411158c3120d982c084e1c6
Ben Hylak <blhylak@wpi.edu>
2018-01-21 13:39:03 -0500

    Created survey activity, and call back interface for them

---
+++ app/src/main/java/org/fundacionparaguaya/advisorapp/activities/AbstractFragSwitcherActivity.java
@@ -0,0 +1,49 @@
+package org.fundacionparaguaya.advisorapp.activities;
+
+import android.app.Activity;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import org.fundacionparaguaya.advisorapp.R;
+import org.fundacionparaguaya.advisorapp.fragments.TabbedFrag;
+import org.fundacionparaguaya.advisorapp.viewcomponents.DashboardTab;
+
+/**
+ * Created by benhylak on 1/21/18.
+ */
+
+public class AbstractFragSwitcherActivity extends AppCompatActivity
+{
+    Fragment mLastFrag;
+
+    protected void switchToFrag(Fragment frag)
+    {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+
+        if(mLastFrag!=null) {
+            ft.detach(mLastFrag);
+        }
+
+        ft.attach(frag).replace(R.id.dash_content, frag).commit();
+
+        mLastFrag = frag;
+    }
+
+
+    /**
+     * This looks confusing, but it is necessary to attach and detach all of the fragments so their placed into
+     * Fragment manager. Otherwise, switching between can be messy.
+     *
+     * @param fragments Frags to add to support fragment manager
+     */
+    public void initTabs(Fragment ... fragments)
+    {
+        for(Fragment frag: fragments)
+        {
+            getSupportFragmentManager().beginTransaction().attach(frag).commit();
+            getSupportFragmentManager().beginTransaction().detach(frag).commit();
+        }
+    }
+
+}
--- app/src/main/java/org/fundacionparaguaya/advisorapp/fragments/callbacks/SurveyFragmentCallbackInterface.java
+++ app/src/main/java/org/fundacionparaguaya/advisorapp/fragments/callbacks/SurveyFragmentCallbackInterface.java
@@ -1,12 +1,32 @@
 package org.fundacionparaguaya.advisorapp.fragments.callbacks;
 
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import org.fundacionparaguaya.advisorapp.models.Snapshot;
+
 /**
  * Created by benhylak on 1/21/18.
  */
 
-public interface SurveyFragmentCallbackInterface
+public interface SurveyFragmentCallbackInterface extends NavigationListener
 {
     void onUpdateProgress(String questionsLeft, int progress);
 
-    void onNavigate(); //should take a fragment or something
+    void setHeaderFooterColor();
+
+    void setTitle();
+
+    void hideHeader();
+
+    void hideFooter();
+
+    void showHeader();
+
+    void showFooter();
+
+    @Nullable
+    LiveData<Snapshot> getSnapshot();
+
+    void onFinish(Snapshot snap);
 }

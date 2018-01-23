ec2551c5a5a63fda1ed0d5f4a6dba707ba588ffa
Ben Hylak <blhylak@wpi.edu>
2018-01-23 09:54:40 -0500

    Resolved some minor details, added a check for an existing snapshot, added a return type to a function that should've been returning something

--- app/src/main/java/org/fundacionparaguaya/advisorapp/viewmodels/SharedSurveyViewModel.java
+++ app/src/main/java/org/fundacionparaguaya/advisorapp/viewmodels/SharedSurveyViewModel.java
@@ -1,10 +1,12 @@
 package org.fundacionparaguaya.advisorapp.viewmodels;
 
 import android.arch.lifecycle.*;
+import android.support.annotation.Nullable;
 import org.fundacionparaguaya.advisorapp.models.*;
 import org.fundacionparaguaya.advisorapp.repositories.FamilyRepository;
 import org.fundacionparaguaya.advisorapp.repositories.SurveyRepository;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -15,6 +17,9 @@ import java.util.List;
 public class SharedSurveyViewModel extends ViewModel
 {
     public enum SurveyState {NONE, INTRO, BACKGROUND_QUESTIONS, INDICATORS, REVIEW}
+
+    static String NO_SNAPSHOT_EXCEPTION_MESSAGE = "Method call requires an existing snapshot, but no snapshot has been created. (Call" +
+            "makeSnapshot before this function";
 
     SurveyRepository mSurveyRepository;
     FamilyRepository mFamilyRepository;
@@ -44,6 +49,8 @@ public class SharedSurveyViewModel extends ViewModel
 
         mSurveyState.setValue(SurveyState.NONE);
         mSnapshot = new MutableLiveData<Snapshot>();
+
+        mSkippedIndicators = new ArrayList<>();
     }
 
     public LiveData<Family> getCurrentFamily()
@@ -93,15 +100,6 @@ public class SharedSurveyViewModel extends ViewModel
     {
         return mSurveyRepository.getSurveys();
     }
-    /**
-     * Gets the current snapshot. Snapshot must have been created with "getNewSnapshot"
-     *
-     * TODO: Remove if this turns out not to be necessary.
-     */
-    public LiveData<Snapshot> getCurrentSnapshot()
-    {
-        return mSnapshot;
-    }
 
     public MutableLiveData<SurveyState> getSurveyState()
     {
@@ -127,27 +125,48 @@ public class SharedSurveyViewModel extends ViewModel
         return mSkippedIndicators;
     }
 
-    public void getResponseForIndicator(IndicatorQuestion question)
+    public @Nullable IndicatorOption getResponseForIndicator(IndicatorQuestion question)
     {
-        mSnapshot.getValue().getIndicatorResponses().get(question);
+        if(hasExistingSnapshot()) {
+            return mSnapshot.getValue().getIndicatorResponses().get(question);
+        }
+
+        return null;
     }
 
     public void addIndicatorResponse(IndicatorQuestion indicator, IndicatorOption response)
     {
-        mSnapshot.getValue().response(indicator, response);
+        if(hasExistingSnapshot())
+        {
+            mSnapshot.getValue().response(indicator, response);
+        }
     }
 
     public void addBackgroundResponse(SurveyQuestion question, String response)
     {
-        if(question instanceof PersonalQuestion)
+        if(hasExistingSnapshot())
         {
-            mSnapshot.getValue().response((PersonalQuestion) question, response);
-        }
-        else if(question instanceof EconomicQuestion)
-        {
-            mSnapshot.getValue().response((EconomicQuestion) question, response);
+            if(question instanceof PersonalQuestion)
+            {
+                mSnapshot.getValue().response((PersonalQuestion) question, response);
+            }
+            else if(question instanceof EconomicQuestion)
+            {
+                mSnapshot.getValue().response((EconomicQuestion) question, response);
+            }
         }
     }
+
+    private boolean hasExistingSnapshot()
+    {
+        if(mSnapshot == null)
+        {
+            throw new IllegalStateException(NO_SNAPSHOT_EXCEPTION_MESSAGE);
+        }
+
+        else return true;
+    }
+
 
     public static class SurveyProgress
     {

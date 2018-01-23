656a7506a8d5e2ccb00b1efd3c596fa805e8815b
melokda <melokda@wpi.edu>
2018-01-23 15:49:32 -0500

    worked on the view component for the background questions

---
+++ app/src/main/java/org/fundacionparaguaya/advisorapp/viewcomponents/BackgroundQuestionsView.java
@@ -0,0 +1,41 @@
+package org.fundacionparaguaya.advisorapp.viewcomponents;
+
+import android.content.Context;
+import android.text.InputType;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.fundacionparaguaya.advisorapp.R;
+
+/**
+ * Created by Mone Elokda on 1/23/2018.
+ */
+
+public class BackgroundQuestionsView extends LinearLayout implements View.OnClickListener{
+
+    private TextView mQuestion;
+    private EditText mAnswer;
+
+
+
+    public BackgroundQuestionsView(Context context) {
+        super(context);
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        inflater.inflate(R.layout.fragment_question_background, this);
+
+        mQuestion = (TextView) findViewById(R.id.background_question);
+        mAnswer = (EditText) findViewById(R.id.answer_text_field);
+
+        mAnswer.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_CLASS_TEXT | );
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}

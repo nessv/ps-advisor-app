b32be5173650b9a109671e2dc42b955b227ca37e
Alex Tacescu <alextac98@gmail.com>
2018-01-23 21:37:28 -0500

    Add file for testing, but not working

--- app/src/main/java/org/fundacionparaguaya/advisorapp/fragments/IndicatorSurveyFragment.java
+++ app/src/main/java/org/fundacionparaguaya/advisorapp/fragments/IndicatorSurveyFragment.java
@@ -3,11 +3,13 @@ package org.fundacionparaguaya.advisorapp.fragments;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import org.fundacionparaguaya.advisorapp.R;
+import org.fundacionparaguaya.advisorapp.adapters.IndicatorAdapter;
 
 /**
  * Created by alex on 1/23/2018.
@@ -15,10 +17,13 @@ import org.fundacionparaguaya.advisorapp.R;
 
 public class IndicatorSurveyFragment extends AbstractSurveyFragment {
 
+    IndicatorAdapter mAdapter;
+
+    ViewPager mPager;
+
     @Override
     public void onCreate(@Nullable Bundle savedInsanceState) {
         super.onCreate(savedInsanceState);
-
 
     }
 
@@ -26,16 +31,24 @@ public class IndicatorSurveyFragment extends AbstractSurveyFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState){
-        View view = inflater.inflate(R.layout.)
+        View view = inflater.inflate(R.layout.fragment_surveyindicatorsurvey, container, false);
+
+        mAdapter = new IndicatorAdapter(getFragmentManager());
+        mPager = (ViewPager) view.findViewById(R.id.indicatorsurvey_viewpager);
+
+        mPager.setAdapter(mAdapter);
+
+        return view;
+    }
+
+    void onSubmit(){
 
     }
 
-
+    public static IndicatorSurveyFragment build(){
+        IndicatorSurveyFragment fragment = new IndicatorSurveyFragment();
+        return fragment;
+    }
 
 }
 
-
-//mAdapter = new DemoFragmentAdapter(getSupportFragmentManager());
-//        mPager = (ViewPager) findViewById(R.id.pager);
-//
-//        mPager.setAdapter(mAdapter);

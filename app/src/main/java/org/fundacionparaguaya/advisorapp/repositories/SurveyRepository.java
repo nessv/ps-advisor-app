dcebd315fd2a85e4661173bcd259c1a4ce8dd60b
Ben Hylak <blhylak@wpi.edu>
2018-01-25 20:23:13 -0500

    Added more dummy indicators, fixed problem in view model with recording skipped indicators

--- app/src/main/java/org/fundacionparaguaya/advisorapp/repositories/SurveyRepository.java
+++ app/src/main/java/org/fundacionparaguaya/advisorapp/repositories/SurveyRepository.java
@@ -57,14 +57,37 @@ public class SurveyRepository {
             indicatorOptions.add(new IndicatorOption("Has no phone.", "https://s3.us-east-2.amazonaws.com/fp-psp-images/25-1.jpg", Red));
             indicatorQuestions.add(new IndicatorQuestion(new Indicator("phone", "Home", indicatorOptions)));
 
+            indicatorOptions = new ArrayList<>();
+            indicatorOptions.add(new IndicatorOption("Another question, green.", "https://s3.us-east-2.amazonaws.com/fp-psp-images/25-3.jpg", Green));
+            indicatorOptions.add(new IndicatorOption("Another question, yellow", "https://s3.us-east-2.amazonaws.com/fp-psp-images/25-2.jpg", Yellow));
+            indicatorOptions.add(new IndicatorOption("Another question, red", "https://s3.us-east-2.amazonaws.com/fp-psp-images/25-1.jpg", Red));
+            indicatorQuestions.add(new IndicatorQuestion(new Indicator("Another Question", "Home", indicatorOptions)));
+
+            indicatorOptions = new ArrayList<>();
+            indicatorOptions.add(new IndicatorOption("Yet another question, green.", "https://s3.us-east-2.amazonaws.com/fp-psp-images/25-3.jpg", Green));
+            indicatorOptions.add(new IndicatorOption("Yet another question, yellow", "https://s3.us-east-2.amazonaws.com/fp-psp-images/25-2.jpg", Yellow));
+            indicatorOptions.add(new IndicatorOption("Yet another question, red", "https://s3.us-east-2.amazonaws.com/fp-psp-images/25-1.jpg", Red));
+            indicatorQuestions.add(new IndicatorQuestion(new Indicator("Yet Another Question", "Home", indicatorOptions)));
+
+            indicatorOptions = new ArrayList<>();
+            indicatorOptions.add(new IndicatorOption("A longer question that goes into a little detail, green", "https://s3.us-east-2.amazonaws.com/fp-psp-images/25-3.jpg", Green));
+            indicatorOptions.add(new IndicatorOption("An even longer question that really gets into the nitty " +
+                    "gritty of the family's situation, yellow", "https://s3.us-east-2.amazonaws.com/fp-psp-images/25-2.jpg", Yellow));
+            indicatorOptions.add(new IndicatorOption("Yet another question that is even longer. extremely long, and infact the longest of the long." +
+                    ", red", "https://s3.us-east-2.amazonaws.com/fp-psp-images/25-1.jpg", Red));
+            indicatorQuestions.add(new IndicatorQuestion(new Indicator("Yet Another Long Question", "Home", indicatorOptions)));
+
+
             List<EconomicQuestion> economicQuestions = new ArrayList<>();
             List<String> economicOptions = new ArrayList<>();
-            economicOptions.add("");
+            economicOptions.add("Employed");
             economicOptions.add("Not Employed");
             economicQuestions.add(new EconomicQuestion("employmentStatus", "Employment status.", ResponseType.String, economicOptions));
 
             List<PersonalQuestion> personalQuestions = new ArrayList<>();
-            personalQuestions.add(new PersonalQuestion("income", "Income.", ResponseType.Integer));
+
+            personalQuestions.add(new PersonalQuestion("name", "What is your name?", ResponseType.String));
+            personalQuestions.add(new PersonalQuestion("income", "What is your annual income?", ResponseType.Integer));
 
             surveyDao.insertSurvey(new Survey(1,personalQuestions, economicQuestions, indicatorQuestions));
         });
--- app/src/main/java/org/fundacionparaguaya/advisorapp/viewmodels/SharedSurveyViewModel.java
+++ app/src/main/java/org/fundacionparaguaya/advisorapp/viewmodels/SharedSurveyViewModel.java
@@ -7,9 +7,7 @@ import org.fundacionparaguaya.advisorapp.models.*;
 import org.fundacionparaguaya.advisorapp.repositories.FamilyRepository;
 import org.fundacionparaguaya.advisorapp.repositories.SurveyRepository;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 /**
  * Survey view model that is shared across all of the fragments/activity related to the view model
@@ -28,7 +26,7 @@ public class SharedSurveyViewModel extends ViewModel
     MutableLiveData<SurveyProgress> mProgress = new MutableLiveData<SurveyProgress>();
     private MutableLiveData<SurveyState> mSurveyState;
 
-    List<IndicatorQuestion> mSkippedIndicators;
+    Set<IndicatorQuestion> mSkippedIndicators;
 
     MutableLiveData<Snapshot> mSnapshot;
 
@@ -50,7 +48,7 @@ public class SharedSurveyViewModel extends ViewModel
         mSurveyState.setValue(SurveyState.NONE);
         mSnapshot = new MutableLiveData<Snapshot>();
 
-        mSkippedIndicators = new ArrayList<>();
+        mSkippedIndicators = new HashSet<>();
     }
 
     public LiveData<Family> getCurrentFamily()
@@ -127,6 +125,7 @@ public class SharedSurveyViewModel extends ViewModel
         //clears any responses for the question
         mSnapshot.getValue().getIndicatorResponses().remove(question);
 
+        //skipped indicators is a hashset, so there will be no duplicate entries.
         mSkippedIndicators.add(question);
 
         calculateProgress();
@@ -146,6 +145,11 @@ public class SharedSurveyViewModel extends ViewModel
     public void addIndicatorResponse(IndicatorQuestion indicator, IndicatorOption response)
     {
         if(response!=null) {
+            if(mSkippedIndicators.contains(indicator))
+            {
+                mSkippedIndicators.remove(indicator);
+            }
+
             getSnapshotValue().response(indicator, response);
 
             calculateProgress();

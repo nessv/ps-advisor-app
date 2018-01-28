2e4402911faa1fae7b5827335db6488767ff14a3
Alex Tacescu <alextac98@gmail.com>
2018-01-28 10:28:10 -0500

    Add layout components

--- app/src/main/java/org/fundacionparaguaya/advisorapp/viewcomponents/SurveySummarySkippedIndicators.java
+++ app/src/main/java/org/fundacionparaguaya/advisorapp/viewcomponents/SurveySummarySkippedIndicators.java
@@ -27,7 +27,7 @@ public class SurveySummarySkippedIndicators extends LinearLayout{
         this.attributeSet = attrs;
 
         LayoutInflater inflater = LayoutInflater.from(context);
-        inflater.inflate(R.layout.surveysummaryskippedindicators_fragment, this);
+        inflater.inflate(R.layout.surveysummary_skipped, this);
 
         textView = (TextView) findViewById(R.id.surveysummary_skippedindicatortext);
     }
---
+++ app/src/main/java/org/fundacionparaguaya/advisorapp/viewcomponents/surveysummary_layout.java
@@ -0,0 +1,10 @@
+package org.fundacionparaguaya.advisorapp.viewcomponents;
+
+/**
+ * Created by alex on 1/28/2018.
+ */
+
+public class surveysummary_layout {
+
+
+}

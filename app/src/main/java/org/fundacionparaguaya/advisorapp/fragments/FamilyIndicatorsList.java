80f78bf792b28a72e38516899434a4eaf3d4f16b
Ben Hylak <blhylak@wpi.edu>
2018-01-29 11:22:03 -0500

    Added drawable for header

--- app/src/main/java/org/fundacionparaguaya/advisorapp/fragments/FamilyIndicatorsList.java
+++ app/src/main/java/org/fundacionparaguaya/advisorapp/fragments/FamilyIndicatorsList.java
@@ -1,10 +1,12 @@
 package org.fundacionparaguaya.advisorapp.fragments;
 
 import android.app.Fragment;
+import org.fundacionparaguaya.advisorapp.models.BackgroundQuestion;
 
 /**
  * List of all the indicators a
  */
 
 public class FamilyIndicatorsList extends Fragment {
+
 }

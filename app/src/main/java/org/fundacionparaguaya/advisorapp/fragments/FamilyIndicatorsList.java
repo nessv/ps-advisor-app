031df502980cb61f8e4ef24bfff9bab99a18ea5f
Ben Hylak <blhylak@wpi.edu>
2018-01-28 19:09:05 -0500

    Added drawables for list view of indicators

---
+++ app/src/main/java/org/fundacionparaguaya/advisorapp/fragments/FamilyIndicatorsList.java
@@ -0,0 +1,10 @@
+package org.fundacionparaguaya.advisorapp.fragments;
+
+import android.app.Fragment;
+
+/**
+ * List of all the indicators a
+ */
+
+public class FamilyIndicatorsList extends Fragment {
+}
--- app/src/main/java/org/fundacionparaguaya/advisorapp/viewmodels/FamilyInformationViewModel.java
+++ app/src/main/java/org/fundacionparaguaya/advisorapp/viewmodels/FamilyInformationViewModel.java
@@ -1,11 +1,17 @@
 package org.fundacionparaguaya.advisorapp.viewmodels;
 
 import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.Transformations;
 import android.arch.lifecycle.ViewModel;
 
-import org.fundacionparaguaya.advisorapp.models.Family;
+import org.fundacionparaguaya.advisorapp.models.*;
 import org.fundacionparaguaya.advisorapp.repositories.FamilyRepository;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 
@@ -13,6 +19,26 @@ public class FamilyInformationViewModel extends ViewModel {
 
     private FamilyRepository mFamilyRepository;
     private LiveData<Family> currentFamily;
+
+    final private MutableLiveData<Snapshot> mSelectedSnapshot = new MutableLiveData<>();
+
+    private LiveData<List<Snapshot>> mSnapshots = Transformations.map(currentFamily, currentFamily -> {
+        if(currentFamily==null)
+        {
+            return null;
+        }
+        else return Arrays.asList(new Snapshot(currentFamily, null));
+    });
+
+    private LiveData<Map<IndicatorQuestion, IndicatorOption>> mIndicatorsForSelected = Transformations.map(mSelectedSnapshot, selected ->
+    {
+        //group by dimension and sort alphabetically, then
+        if(selected==null)
+        {
+            return null;
+        }
+        else return selected.getIndicatorResponses();
+    });
 
     public  FamilyInformationViewModel(FamilyRepository familyRepository){
         mFamilyRepository = familyRepository;
@@ -36,5 +62,15 @@ public class FamilyInformationViewModel extends ViewModel {
             throw new IllegalStateException("setFamily must be called in ViewModel before getCurrentFamily");
         }
         else return currentFamily;
+    }
+
+    public LiveData<List<Snapshot>> getSnapshots()
+    {
+        return mSnapshots;
+    }
+
+    public void setSelectedSnapshot(Snapshot s)
+    {
+        mSelectedSnapshot.setValue(s);
     }
 }

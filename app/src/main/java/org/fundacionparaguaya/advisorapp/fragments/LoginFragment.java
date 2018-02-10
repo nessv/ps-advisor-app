65cc4075833f8e47b7bf56c069c00e5745fae2ee
Ben Hylak <blhylak@wpi.edu>
2018-02-10 00:27:30 -0500

    Removed instabug to use same setup

--- app/src/main/java/org/fundacionparaguaya/advisorapp/fragments/LoginFragment.java
+++ app/src/main/java/org/fundacionparaguaya/advisorapp/fragments/LoginFragment.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Rect;
 import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;

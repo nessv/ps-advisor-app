1cc1dd8e2e2b5ba3ff7482c5cf20fff1bf31b080
Alex Tacescu <alextac98@gmail.com>
2018-02-11 14:37:36 -0500

    merge devel into current branch

--- app/src/main/java/org/fundacionparaguaya/advisorapp/fragments/SurveyQuestionsFrag.java
+++ app/src/main/java/org/fundacionparaguaya/advisorapp/fragments/SurveyQuestionsFrag.java
@@ -46,6 +46,7 @@ public abstract class SurveyQuestionsFrag extends AbstractSurveyFragment impleme
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override

8e8a130975d1399e23cd1c34b8d55dc47b3073b7
Alex Tacescu <alextac98@gmail.com>
2018-02-13 00:35:08 -0500

    Add Choose survey option in Survey Page

--- app/src/main/java/org/fundacionparaguaya/advisorapp/fragments/SurveyIntroFragment.java
+++ app/src/main/java/org/fundacionparaguaya/advisorapp/fragments/SurveyIntroFragment.java
@@ -4,17 +4,25 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+
 import com.ontbee.legacyforks.cn.pedant.SweetAlert.SweetAlertDialog;
+
 import org.fundacionparaguaya.advisorapp.AdvisorApplication;
 import org.fundacionparaguaya.advisorapp.R;
+import org.fundacionparaguaya.advisorapp.adapters.SurveyListAdapter;
 import org.fundacionparaguaya.advisorapp.models.Survey;
 import org.fundacionparaguaya.advisorapp.viewmodels.InjectionViewModelFactory;
 import org.fundacionparaguaya.advisorapp.viewmodels.SharedSurveyViewModel;
-import org.fundacionparaguaya.advisorapp.viewmodels.SharedSurveyViewModel.*;
+import org.fundacionparaguaya.advisorapp.viewmodels.SharedSurveyViewModel.SurveyState;
+
+import java.util.ArrayList;
 
 import javax.inject.Inject;
 
@@ -22,26 +30,32 @@ import javax.inject.Inject;
  * Intro page on a new survey
  */
 
-public class SurveyIntroFragment extends AbstractSurveyFragment
-{
-    static String FRAGMENT_TAG = "SurveyIntroFragment";
+public class SurveyIntroFragment extends AbstractSurveyFragment {
+    private static String FRAGMENT_TAG = "SurveyIntroFragment";
 
     @Inject
     InjectionViewModelFactory mViewModelFactory;
 
-    SharedSurveyViewModel mSurveyViewModel;
+    private SharedSurveyViewModel mSurveyViewModel;
+
+    private RecyclerView mSurveyOptionList;
+    private Button mSubmitButton;
+
+    private ArrayList<Survey> mSurveyList;
+
+    private SurveyListAdapter mAdapter;
+
+    private Survey selectedSurvey = null;
 
     //need the family name
 
-    public SurveyIntroFragment()
-    {
+    public SurveyIntroFragment() {
         setShowFooter(false);
         setShowHeader(false);
     }
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState)
-    {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         //Inject view model factory and load view model
@@ -63,57 +77,96 @@ public class SurveyIntroFragment extends AbstractSurveyFragment
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState)
-    {
+                             @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_surveyintro, container, false);
 
-        view.findViewById(R.id.btn_surveyintro_submit).setOnClickListener((event)->onSubmit());
-
         TextView familyNameTv = view.findViewById(R.id.tv_surveyintro_familyname);
+        mSubmitButton = view.findViewById(R.id.btn_surveyintro_submit);
+
+        mSubmitButton.setOnClickListener((event) -> onSubmit());
+
+        mSurveyOptionList = (RecyclerView) view.findViewById(R.id.surveyintro_surveyoptionlist);
+        mSurveyOptionList.setLayoutManager(new LinearLayoutManager(getContext()));
 
         mSurveyViewModel.getCurrentFamily().observe(this, (family ->
                 familyNameTv.setText(family.getName() + " Family")));
 
+        mAdapter = new SurveyListAdapter(getContext(), mSurveyList);
+        mSurveyOptionList.setAdapter(mAdapter);
+        mAdapter.setClickListener(this::onItemClick);
+
+
         return view;
     }
 
-    void onSubmit(){
-
+    @Override
+    public void onResume() {
+        super.onResume();
         mSurveyViewModel.getSurveys().observe(this, (surveys) ->
         {
-            if(surveys.size()>0) {
-                Survey survey = surveys.get(0);
-
-                mSurveyViewModel.makeSnapshot(survey); //assumes family livedata object has value
-
-                /**when snapshot is made**/
-                mSurveyViewModel.getSnapshot().observe(this, (snapshot -> {
-                    mSurveyViewModel.setSurveyState(SurveyState.ECONOMIC_QUESTIONS);
-                }));
-            }
-            else
-            {
-                new SweetAlertDialog(getContext(), SweetAlertDialog.ERROR_TYPE)
-                        .setTitleText(getString(R.string.survey_error_no_surveys_title))
-                        .setContentText(getString(R.string.survey_error_no_surveys_description))
-                        .setConfirmText(getString(R.string.all_okay))
-                        .setConfirmClickListener((dialog)->
-                        {
-                            getActivity().finish();
-                        })
-                        .show();
-            }
-            //create snapshot with family and
+            mSurveyList = (ArrayList<Survey>) surveys;
+            mAdapter.setSurveyList(mSurveyList);
+            mSurveyOptionList.setAdapter(mAdapter);
         });
+
     }
 
-    public static String getFragmentTag()
-    {
+    private void onItemClick(Survey survey, boolean isSelected) {
+        if (isSelected) {
+            if (selectedSurvey != null){
+                for(Survey loopSurvey : mAdapter.getSurveyList()){
+                    if (!loopSurvey.equals(survey)){
+                        //Unselect all who are not selected currently
+                        mAdapter.getViewHolderHashMap().get(loopSurvey).setSelected(false);
+                    }
+                }
+                selectedSurvey = null;
+            }
+            selectedSurvey = survey;
+            mSubmitButton.setVisibility(View.VISIBLE);
+        } else {
+            selectedSurvey = null;
+            mSubmitButton.setVisibility(View.GONE);
+        }
+    }
+
+    void onSubmit() {
+        if (selectedSurvey != null) {
+            mSurveyViewModel.makeSnapshot(selectedSurvey); //assumes family livedata object has value
+
+            /**when snapshot is made**/
+            mSurveyViewModel.getSnapshot().observe(this, (snapshot -> {
+                mSurveyViewModel.setSurveyState(SurveyState.ECONOMIC_QUESTIONS);
+            }));
+        } else {
+//            new SweetAlertDialog(getContext(), SweetAlertDialog.ERROR_TYPE)
+//                    .setTitleText(getString(R.string.survey_error_no_surveys_title))
+//                    .setContentText(getString(R.string.survey_error_no_surveys_description))
+//                    .setConfirmText(getString(R.string.all_okay))
+//                    .setConfirmClickListener((dialog) -> {
+//                        getActivity().finish();
+//                    })
+//                    .show();
+        }
+
+        if (mSurveyList.size() == 0) {
+            new SweetAlertDialog(getContext(), SweetAlertDialog.ERROR_TYPE)
+                    .setTitleText(getString(R.string.survey_error_no_surveys_title))
+                    .setContentText(getString(R.string.survey_error_no_surveys_description))
+                    .setConfirmText(getString(R.string.all_okay))
+                    .setConfirmClickListener((dialog) ->
+                    {
+                        getActivity().finish();
+                    })
+                    .show();
+        }
+    }
+
+    public static String getFragmentTag() {
         return FRAGMENT_TAG;
     }
 
-    public static SurveyIntroFragment build()
-    {
+    public static SurveyIntroFragment build() {
         SurveyIntroFragment fragment = new SurveyIntroFragment();
 
         return fragment;

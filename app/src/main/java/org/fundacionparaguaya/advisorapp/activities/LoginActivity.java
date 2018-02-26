6e2bec872e8045e02eef9f3b6aa984ec7c253f48
melokda <melokda@wpi.edu>
2018-02-26 14:17:15 -0500

    fix codacy problems

--- app/src/main/java/org/fundacionparaguaya/advisorapp/activities/LoginActivity.java
+++ app/src/main/java/org/fundacionparaguaya/advisorapp/activities/LoginActivity.java
@@ -9,6 +9,7 @@ import android.view.MotionEvent;
 import com.instabug.library.InstabugTrackingDelegate;
 import org.fundacionparaguaya.advisorapp.R;
 import org.fundacionparaguaya.advisorapp.fragments.LoginFragment;
+import org.fundacionparaguaya.advisorapp.util.Utilities;
 
 /**
  * A login screen that offers login via email/password.
@@ -36,6 +37,8 @@ public class LoginActivity extends AppCompatActivity {
         FragmentTransaction transaction = manager.beginTransaction();
         transaction.replace(R.id.login_root, loginFragment, LOGIN_FRAG_TAG);
         transaction.commit();
+
+        Utilities.isGooglePlayServicesAvailable(this);
     }
 }
 

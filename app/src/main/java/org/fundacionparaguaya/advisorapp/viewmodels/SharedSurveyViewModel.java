342120e61e9edf08eb32885cfcef0ef3d141d9bd
Ben Hylak <blhylak@wpi.edu>
2018-03-14 12:23:05 -0400

    Removed kotlin plugin

--- app/src/main/java/org/fundacionparaguaya/advisorapp/viewmodels/SharedSurveyViewModel.java
+++ app/src/main/java/org/fundacionparaguaya/advisorapp/viewmodels/SharedSurveyViewModel.java
@@ -155,13 +155,6 @@ public class SharedSurveyViewModel extends ViewModel {
         return mSurvey;
     }
 
-    public boolean isRequirementMet(BackgroundQuestion q) {
-        String response = getBackgroundResponse(q);
-
-        boolean requirementsMet = !(q.isRequired() && (response == null || response.isEmpty()));
-        return requirementsMet;
-    }
-
     /**
      * Returns the surveys available to take.
      */
